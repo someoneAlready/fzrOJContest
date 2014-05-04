c3f302b1f372694fc2cb16f32faf249f4f1cc5ee
cgangEE <cgangEE@gmail.com>
2014-05-04 10:22:02 +0800

    .

--- tools/begin.php
+++ tools/begin.php
@@ -1,7 +1,7 @@
+<?php session_start(); ?>
 <?php
 	require_once 'tools/info.php';
-
-	if ($start_time>time()){
+	if ($start_time>time() && ($_SESSION['uadmin']!=1)){
 		header("Location:index.php");
 		exit();
 	}
